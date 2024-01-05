4991729e508704536de6a85fb8f62a57765cb10f
Mursheed <mursheed.khan@qsstechnosoft.com>
2024-01-05 23:25:59 +0530

    ecommerce backend

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+require('./db/config');
+const cors = require("cors");
+const User = require('./db/User');
+const Product = require('./db/Product');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/register', async (req,resp)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+});
+
+app.post('/login', async (req,resp)=>{
+    if(req.body.password && req.body.email)
+    {
+        let user = await User.findOne(req.body).select("-password");
+        if(user){
+            resp.send(user);
+        }else{
+            resp.send({result:"No User Found"});
+        }
+    }else{
+        resp.send({ result:"Either email or password not found"});
+    }
+    
+    
+})
+app.post('/add-product', async (req,resp)=>{
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
+})
+
+app.get('/products', async (req,resp)=>{
+    
+    let result = await Product.find();
+    if(result.length > 0){
+        resp.send(result);
+    }else{
+        resp.send({result:"No resut found"});
+    }
+    
+})
+
+
+app.listen(5000);
