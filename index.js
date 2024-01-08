701761d71745ca3c63f94fe844f0a574fb6d35a3
Mursheed <mursheed.khan@qsstechnosoft.com>
2024-01-08 15:53:36 +0530

    commi 4 more APIs

--- index.js
+++ index.js
@@ -49,5 +49,53 @@ app.get('/products', async (req,resp)=>{
     
 })
 
+app.delete('/product/:id', async (req,resp)=>{
+    
+    let result = await Product.deleteOne({_id:req.params.id});
+    resp.send(result);
+    
+    
+})
+
+app.get('/product/:id', async (req,resp)=>{
+    
+    let result = await Product.findOne({_id:req.params.id});
+    
+    if(result){
+        resp.send(result);
+    }else{
+        resp.send({result:"No resut found"});
+    }
+    
+})
+
+app.put('/product/:id', async (req,resp)=>{
+    
+    let result = await Product.updateOne(
+            {_id:req.params.id},
+            {
+                $set : req.body
+            }
+        
+        );
+    
+    if(result){
+        resp.send(result);
+    }else{
+        resp.send({result:"No resut found"});
+    }
+    
+})
+
+app.get('/search/:key', async (req,resp)=>{
+    let result = await Product.find({
+        "$or":[
+            {name:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}}
+        ]
+    });
+    resp.send(result);
+})
+
 
 app.listen(5000);
